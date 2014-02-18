94d9c1d10817a250c8c815aa446924e743707555
Ross Donaldson <gastove@gmail.com>
2014-02-17 20:25:03 -0800

    Adding lein structures -- doc, project, etc.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ufoh
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/ufoh/core_test.clj
@@ -0,0 +1,7 @@
+(ns ufoh.core-test
+  (:require [clojure.test :refer :all]
+            [ufoh.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
