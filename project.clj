dd121ad6147f96b9031d1d9676ec07f1324c7cab
Ross Donaldson <gastove@gmail.com>
2014-02-25 23:26:16 -0800

    Got a load process working -- the data is operable. We are live.

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[incanter "1.5.4"]
-                 [org.clojure/clojure "1.5.1"]]
+                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :main ^:skip-aot ufoh.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :jvm-opts ["-Xms128m" "-Xmx8192m"]
+  :Profiles {:uberjar {:aot :all}})
---
+++ src/ufoh/load-and-clean-data.clj
@@ -0,0 +1,18 @@
+(ns load-and-clean
+  (:use [incanter core io]))
+
+(defn load-clean [path]
+  (let [data (read-dataset path :delim \tab)
+        columns (col-names data)
+        good-cols (take 5 columns)
+        bad-cols (drop 5 columns)
+        cleanup (fn [& args] (apply str args))]
+    (->> ($map cleanup bad-cols data)
+         (conj-cols ($ good-cols data)))))
+
+(defn get-data []
+  (let [data (load-clean "./lib/ufo_awesome.tsv")
+        headers [:sighted_at :reported_at :location :shape :duration :description]
+        ]
+    (col-names data headers)
+    ))
